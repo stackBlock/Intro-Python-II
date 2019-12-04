aef3210dc3c42276e466d9ebe9e1bae23ccba936
stackblock <lambdaTL.anthony@gmail.com>
2019-12-03 19:17:32 -0500

    finished

--- src/adv.py
+++ src/adv.py
@@ -1,11 +1,13 @@
+import os
 from room import Room
+from player import Player
+from item import Item
 
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
                      "North of you, the cave mount beckons"),
-
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
 
@@ -21,9 +23,7 @@ chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
 
-
 # Link rooms together
-
 room['outside'].n_to = room['foyer']
 room['foyer'].s_to = room['outside']
 room['foyer'].n_to = room['overlook']
@@ -33,19 +33,82 @@ room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
-#
+# items and rooms w/items
+
+knife = Item("knife", "knife made from the claw of a tiger")
+potion = Item("potion", "The potion retores +10 hit points")
+book_of_spells = Item("book_of_spells", "little black book_of_spells")
+
+room['overlook'].items = [potion]
+room['treasure'].items = [knife]
+room['narrow'].items = [book_of_spells]
+
 # Main
-#
 
-# Make a new player object that is currently in the 'outside' room.
+player_name = input("Welcome to the game of passages. What is your name?: ")
+new_player = Player(player_name, room['outside'])
 
-# Write a loop that:
-#
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
-# * Waits for user input and decides what to do.
-#
-# If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
-#
-# If the user enters "q", quit the game.
+print(f"Current location: { new_player.players_room.name }\n")
+
+print(new_player.players_room.description)
+
+def get_an_item():
+    item_choice = input("What do you want to do? (enter 'take' or 'leave'): ")
+
+    for i in new_player.players_room.items:
+        if item_choice == "take":
+            new_player.items.append(i)
+            new_player.players_room.items.remove(i)
+        elif item_choice =="leave":
+            print(f"You left the {i} behind.\n")
+        elif item_choice == 'q':
+            print("Come back and play again.")
+            os._exit(0)
+        else:
+            print("Option not available.\n")
+            get_an_item()
+
+def new_location():
+    print(new_player.players_room)
+    if len(new_player.players_room.items) > 0:
+        get_an_item()
+        print(f"\n{new_player} \n")
+
+def wrong_direction():
+    print("You can not go in that direction - try again...\n")
+    
+user_choice = ''
+while user_choice != 'q':
+    user_choice = input("Please pick a direction (n, s, e, w): ").lower()
+    if user_choice == 'n':
+        try:
+            new_player.players_room.n_to
+            new_player.players_room = new_player.players_room.n_to
+            new_location()
+        except:
+            wrong_direction()
+    elif user_choice == 's':
+        try:
+            new_player.players_room.s_to
+            new_player.players_room = new_player.players_room.s_to
+            new_location()
+        except:
+            wrong_direction()
+    elif user_choice == 'e':
+        try:
+            new_player.players_room
+            new_player.players_room = new_player.players_room.e_to
+            new_location()
+        except:
+            wrong_direction()
+    elif user_choice == 'w':
+        try:
+            new_player.players_room
+            new_player.players_room = new_player.players_room.w_to
+            new_location()
+        except:
+            wrong_direction()
+    elif user_choice == 'q':
+        print("Come back and play again.")
+    else:
+        print("Pick again, something went wrong...\n")
--- src/player.py
+++ src/player.py
@@ -1,2 +1,15 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, name, players_room):
+        self.name = name
+        self.players_room = players_room
+        self.items = []
+        
+    def __str__(self):
+        stuff = f"{self.name}\n Inventory: "
+
+        for i in self.items:
+            stuff += f"{i} "
+        return stuff
